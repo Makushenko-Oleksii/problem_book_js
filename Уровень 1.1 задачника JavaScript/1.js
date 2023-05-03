84ff5257b7f33948c6e57eb9c5573e511640537e
Makushenko Oleksii <lexmak80@gmail.com>
2023-05-03 21:06:20 +0300

    add first task

---
+++ Уровень 1.1 задачника JavaScript/1.js
@@ -0,0 +1,8 @@
+//Дано число. Проверьте, отрицательное оно или нет. Выведите об этом информацию в консоль
+
+let someNum = 4;
+if(someNum > 0) {
+    console.log('positive integer');
+}else {
+    console.log('negative integer' );
+}
